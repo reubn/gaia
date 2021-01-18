0c95099552c32c037dbc1f43e8ddc37065194a19
Reuben <4957964+reubn@users.noreply.github.com>
2021-01-18 11:37:21 +0000

    🔥 Disable Bottom Panel Snap

--- Gaia/MapView/MapViewController.swift
+++ Gaia/MapView/MapViewController.swift
@@ -141,8 +141,7 @@ class MapPopoverPanelLayout: FloatingPanelLayout {
   var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
     return [
         .full: FloatingPanelLayoutAnchor(absoluteInset: 16.0, edge: .top, referenceGuide: .safeArea),
-        .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea),
-        .tip: FloatingPanelLayoutAnchor(absoluteInset: 44.0, edge: .bottom, referenceGuide: .safeArea),
+        .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea)
         ]
   }
   
