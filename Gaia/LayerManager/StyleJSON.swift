91808abdd51905da262ea902a8d5e891ea36f722
Reuben <4957964+reubn@users.noreply.github.com>
2021-02-03 20:54:57 +0000

    🔥 AnyCodable

--- Gaia/LayerManager/StyleJSON.swift
+++ Gaia/LayerManager/StyleJSON.swift
@@ -1,29 +1,12 @@
 import Foundation
 
+import AnyCodable
+
 struct StyleJSON: Codable {
   var version = 8
-  let sources: [String: Source]
-  let layers: [Layer]
-
-  struct Source: Codable {
-    let type: LayerType
-    let tiles: [String]
-
-    let minzoom: Int?
-    let maxzoom: Int?
-    let tileSize: Int?
-  }
+  let sources: [String: AnyCodable]
+  let layers: [AnyCodable]
   
-  struct Layer: Codable {
-    let id: String
-    let type: LayerType
-    let source: String
-
-    let minzoom: Int?
-    let maxzoom: Int?
-  }
-  
-  enum LayerType: String, Codable {
-    case vector, raster
-  }
+  typealias Source = AnyCodable
+  typealias Layer = AnyCodable
 }
