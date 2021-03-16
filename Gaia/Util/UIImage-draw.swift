3a0f9f3fbe8054df93194a169d33c4933387d87d
Reuben <4957964+reubn@users.noreply.github.com>
2021-03-16 22:04:08 +0000

    🔥 Center Images on Largest

--- Gaia/Util/UIImage-draw.swift
+++ Gaia/Util/UIImage-draw.swift
@@ -4,14 +4,16 @@ import UIKit
 extension UIImage {
   func draw(inFrontOf: UIImage) -> UIImage {
     let compositeImage: UIImage!
-
-    let size = CGSize(width: self.size.width, height: self.size.height)
-    let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
     
-    UIGraphicsBeginImageContext(size)
+    let maxWidth = max(self.size.width, inFrontOf.size.width)
+    let maxHeight = max(self.size.height, inFrontOf.size.height)
+
+    let maxSize = CGSize(width: maxWidth, height: maxHeight)
+    
+    UIGraphicsBeginImageContext(maxSize)
           
-    inFrontOf.draw(in: rect)
-    self.draw(in: rect)
+    inFrontOf.draw(at: CGPoint(x: (maxWidth / 2) - (inFrontOf.size.width / 2), y: (maxHeight / 2) - (inFrontOf.size.height / 2)))
+    self.draw(at: CGPoint(x: (maxWidth / 2) - (size.width / 2), y: (maxHeight / 2) - (size.height / 2)))
 
     compositeImage = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
