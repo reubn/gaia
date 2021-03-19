df3a327b4eff3cc5faf4f7f43bc293fb9c8422d1
Reuben <4957964+reubn@users.noreply.github.com>
2021-03-19 20:32:41 +0000

    🔥 Sensible setVisibleCoordinateBounds

---
+++ Gaia/Util/MGLMapView.swift
@@ -0,0 +1,18 @@
+import Mapbox
+
+extension MGLMapView {
+  public func setVisibleCoordinateBounds(_ bounds: MGLCoordinateBounds, sensible: Bool, animated: Bool){
+    if(!sensible) {
+      return setVisibleCoordinateBounds(bounds, animated: animated)
+    }
+    
+    let currentSpan = visibleCoordinateBounds.span
+    let boundsSpan = bounds.span
+    
+    if(boundsSpan.latitudeDelta <= currentSpan.latitudeDelta && boundsSpan.longitudeDelta <= currentSpan.longitudeDelta){
+      setCenter(bounds.center, animated: animated) // can fit bounds at current zoom so center bounds
+    } else {
+      setVisibleCoordinateBounds(bounds, animated: animated) // else fit bounds normally
+    }
+  }
+}
