a465874248cd121c3d26db72ff3d514a5883d777
Reuben <4957964+reubn@users.noreply.github.com>
2021-03-29 21:47:21 +0100

    🔥Get More Screen Space

--- Gaia/LayerSelectView/LayerSelectPanelLayout.swift
+++ Gaia/LayerSelectView/LayerSelectPanelLayout.swift
@@ -8,6 +8,6 @@ let layerSelectPanelLayout = MapViewPanelLayout(
   initialState: .half,
   anchors: [
     .full: FloatingPanelLayoutAnchor(absoluteInset: 16.0, edge: .top, referenceGuide: .safeArea),
-    .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea)
+    .half: FloatingPanelLayoutAnchor(fractionalInset: 0.6, edge: .bottom, referenceGuide: .safeArea)
   ]
 )
