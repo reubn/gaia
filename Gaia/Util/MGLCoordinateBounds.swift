b62ff2aee8e5b7231fb54c65eadeaf9a838b6deb
Reuben <4957964+reubn@users.noreply.github.com>
2021-04-14 09:38:28 +0100

    🔥 MGLCoordinateBounds Codable

--- Gaia/Util/MGLCoordinateBounds.swift
+++ Gaia/Util/MGLCoordinateBounds.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Mapbox
 
-extension MGLCoordinateBounds: Equatable, Hashable {
+extension MGLCoordinateBounds: Codable, Equatable, Hashable {
   public static func == (lhs: Self, rhs: Self) -> Bool {
     MGLCoordinateBoundsEqualToCoordinateBounds(lhs, rhs)
   }
@@ -33,5 +33,24 @@ extension MGLCoordinateBounds: Equatable, Hashable {
   
   public func intersects(with: Self) -> Bool {
     MGLCoordinateBoundsIntersectsCoordinateBounds(self, with)
+  }
+  
+  enum CodingKeys: String, CodingKey {
+    case sw
+    case ne
+  }
+  
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(sw, forKey: .sw)
+    try container.encode(ne, forKey: .ne)
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let sw = try container.decode(CLLocationCoordinate2D.self, forKey: .sw)
+    let ne = try container.decode(CLLocationCoordinate2D.self, forKey: .ne)
+
+    self.init(sw: sw, ne: ne)
   }
 }
