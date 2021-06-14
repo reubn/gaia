ff95b56c0cdd3be8438e81f50bf7061a731321a4
Reuben <4957964+reubn@users.noreply.github.com>
2021-06-14 17:31:04 +0100

    🐛 Screen Cutoff

--- ios/Gaia/OfflineSelectView/OfflineSelectPanelLayout.swift
+++ ios/Gaia/OfflineSelectView/OfflineSelectPanelLayout.swift
@@ -9,6 +9,6 @@ let offlineSelectPanelLayout = PanelLayout(
   anchors: [
     .full: FloatingPanelLayoutAnchor(absoluteInset: 16.0, edge: .top, referenceGuide: .safeArea),
     .half: FloatingPanelLayoutAnchor(fractionalInset: 0.5, edge: .bottom, referenceGuide: .safeArea),
-    .tip: FloatingPanelLayoutAnchor(absoluteInset: 44.0, edge: .bottom, referenceGuide: .safeArea)
+    .tip: FloatingPanelLayoutAnchor(absoluteInset: 60.0, edge: .bottom, referenceGuide: .safeArea)
   ]
 )
