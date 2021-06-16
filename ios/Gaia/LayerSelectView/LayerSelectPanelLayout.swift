6e3e0eefa45b9ddc3ece9046058f0163bdde1c6b
Reuben <4957964+reubn@users.noreply.github.com>
2021-06-16 06:59:05 +0100

    🔥 Allow Snapping LayerSelect to Bottom

--- ios/Gaia/LayerSelectView/LayerSelectPanelLayout.swift
+++ ios/Gaia/LayerSelectView/LayerSelectPanelLayout.swift
@@ -8,6 +8,7 @@ let layerSelectPanelLayout = PanelLayout(
   initialState: .half,
   anchors: [
     .full: FloatingPanelLayoutAnchor(absoluteInset: 16.0, edge: .top, referenceGuide: .safeArea),
-    .half: FloatingPanelLayoutAnchor(fractionalInset: 0.6, edge: .bottom, referenceGuide: .safeArea)
+    .half: FloatingPanelLayoutAnchor(fractionalInset: 0.6, edge: .bottom, referenceGuide: .safeArea),
+    .tip: FloatingPanelLayoutAnchor(absoluteInset: 60.0, edge: .bottom, referenceGuide: .safeArea)
   ]
 )
