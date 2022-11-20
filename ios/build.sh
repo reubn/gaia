1584b7b1b36e1736364740841df574a4287b87c5
Reuben <4957964+reubn@users.noreply.github.com>
2022-11-20 14:14:27 +0000

    🐛

--- ios/build.sh
+++ ios/build.sh
@@ -3,7 +3,7 @@
 tempdir=$(mktemp -d)
 tempfile=$tempdir/archive.xcarchive
 
-xcodebuild -quiet -project ./Gaia.xcodeproj -config Release -scheme Gaia -archivePath $tempfile archive
+xcodebuild -allowProvisioningUpdates -quiet -project ./Gaia.xcodeproj -config Release -scheme Gaia -archivePath $tempfile archive
 mv "$tempfile/Products/Applications" "$tempfile/Payload"
 
 
