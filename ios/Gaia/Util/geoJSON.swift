6d75aeaf608b9cbbfe4fa9255024aa44bf22face
Reuben <4957964+reubn@users.noreply.github.com>
2022-11-28 20:21:14 +0000

    🔥 Convert AnyCodable GeoJSON to Turf GeoJSON

--- ios/Gaia/Util/geoJSON.swift
+++ ios/Gaia/Util/geoJSON.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Turf
 import Mapbox
 
 public func geoJSON(appearsToBe object: AnyCodable) -> Bool {
@@ -73,4 +74,125 @@ typealias GeoJSONCoord = [CLLocationDegrees]
 
 fileprivate func castCoord(_ coordinates: AnyCodable?) -> GeoJSONCoord? {
   return coordinates?.value as? [NSNumber] as? [CLLocationDegrees]
+}
+
+extension Geometry {
+  static func from(anyCodable: AnyCodable?) -> GeometryConvertible? {
+    guard
+      let type = anyCodable?.type?.value as? String,
+      let coordinates = anyCodable?.coordinates
+    else {return nil}
+    
+    switch type {
+      case "Point":
+        if let coordinates = CLLocationCoordinate2D(anyCodable: coordinates){
+          return Point(coordinates)
+        }
+
+      case "LineString":
+        let coordinates = coordinates.compactMap({CLLocationCoordinate2D(anyCodable: $0)})
+        return LineString(coordinates)
+        
+      case "MultiPoint":
+        let coordinates = coordinates.compactMap({CLLocationCoordinate2D(anyCodable: $0)})
+        return MultiPoint(coordinates)
+        
+      case "Polygon":
+        let coordinates = coordinates.map({ring in ring.compactMap({CLLocationCoordinate2D(anyCodable: $0)})})
+        return Polygon(coordinates)
+        
+      case "MultiLineString":
+        let coordinates = coordinates.map({lineString in lineString.compactMap({CLLocationCoordinate2D(anyCodable: $0)})})
+        return MultiLineString(coordinates)
+        
+      case "MultiPolygon":
+        let coordinates = coordinates.map({polygon in polygon.map({ring in ring.compactMap({CLLocationCoordinate2D(anyCodable: $0)})})})
+        return MultiPolygon(coordinates)
+      default: print("Unhandled Geometry"); return nil
+    }
+    return nil
+  }
+}
+
+extension JSONValue {
+  static func from(anyCodable: AnyCodable?) -> JSONValue? {
+    switch anyCodable?.value {
+      case let value as Bool: return .boolean(value)
+      case let value as Int: return .number(Double(value))
+      case let value as Int8: return .number(Double(value))
+      case let value as Int16: return .number(Double(value))
+      case let value as Int32: return .number(Double(value))
+      case let value as Int64: return .number(Double(value))
+      case let value as UInt: return .number(Double(value))
+      case let value as UInt8: return .number(Double(value))
+      case let value as UInt16: return .number(Double(value))
+      case let value as UInt32: return .number(Double(value))
+      case let value as UInt64: return .number(Double(value))
+      case let value as Float: return .number(Double(value))
+      case let value as Double: return .number(value)
+      case let value as String: return .string(value)
+      case let value as [String: AnyCodable]:
+        if let jsonObject = JSONObject.from(anyCodable: AnyCodable(value)) {
+          return .object(jsonObject)
+        }
+      case let value as [AnyCodable]: return .array(JSONArray(value.compactMap({JSONValue.from(anyCodable: $0)})))
+      case let value as AnyCodable: return JSONValue.from(anyCodable: value)
+      case let value as [String: Any]:
+        if let jsonObject = JSONObject.from(anyCodable: AnyCodable(value.mapValues({AnyCodable($0)}))) {
+          return .object(jsonObject)
+        }
+      case let value as [Any]:  return .array(JSONArray(value.compactMap({JSONValue.from(anyCodable: AnyCodable($0))})))
+        
+      default: return nil
+    }
+    
+    return nil
+  }
+}
+
+extension JSONObject {
+  static func from(anyCodable: AnyCodable?) -> JSONObject? {
+    if let anyCodable = anyCodable, let object = anyCodable.value as? [String: Any] {
+      let dictionary = object.map({($0.key, JSONValue.from(anyCodable: AnyCodable($0.value)))})
+      return JSONObject(uniqueKeysWithValues: dictionary)
+    }
+    
+    return nil
+  }
+}
+
+extension GeoJSONObjectConvertible {
+  static func from(anyCodable: AnyCodable?) -> GeoJSONObjectConvertible? {
+    guard
+      let anyCodable = anyCodable,
+      let type = anyCodable.type?.value as? String
+    else {return nil}
+
+    switch type {
+      case "FeatureCollection":
+        let features = anyCodable.features?.compactMap({GeoJSONObject.from(anyCodable: $0) as? Feature}) ?? []
+        return FeatureCollection(features: features)
+
+      case "Feature":
+        let geometry = Geometry.from(anyCodable: anyCodable.geometry)
+        var feature = Feature(geometry: geometry)
+
+        feature.identifier = FeatureIdentifier(rawValue: anyCodable.id?.value as? String)
+        feature.properties = JSONObject.from(anyCodable: anyCodable.properties)
+        
+        return feature
+
+      default: return Geometry.from(anyCodable: anyCodable)
+    }
+  }
+}
+
+extension GeoJSONObjectConvertible {
+  func flattenFeatures() -> [Feature] {
+    switch self.geoJSONObject {
+      case .geometry: return []
+      case .feature(let feature): return [feature]
+      case .featureCollection(let featureCollection): return featureCollection.features.flatMap({$0.flattenFeatures()})
+    }
+  }
 }
